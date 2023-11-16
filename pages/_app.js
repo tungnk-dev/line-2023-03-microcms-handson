b62c2c942cc7b65225db71a4a8032245c06c38f3
tungnk-dev <63533411+tungnk-dev@users.noreply.github.com>
2023-11-16 12:53:25 +0000

    lst upd

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,70 @@ import { useEffect, useState, createContext } from "react";
 
 export const LiffContext = createContext({});
 
+const Counter = ({initialCount}) => {
+  const [count, setCount] = useState(initialCount);
+  return (
+    <>
+      Count: {count}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+    </>
+  );
+}
+
+useEffect(
+  () => {
+    updateData()
+    setLoad(false)
+  },
+  [load],
+);
+
+const themes = {
+  light: {
+    foreground: "#000000",
+    background: "#eeeeee"
+  },
+  dark: {
+    foreground: "#ffffff",
+    background: "#222222"
+  }
+};
+
+const ThemeContext = React.createContext(themes.light);
+
+function App() {
+  return (
+    <ThemeContext.Provider value={themes.dark}>
+      <Toolbar />
+    </ThemeContext.Provider>
+  );
+}
+
+function Toolbar(props) {
+  return (
+    <div>
+      <ThemedButton />
+    </div>
+  );
+}
+
+function ThemedButton() {
+  const theme = useContext(ThemeContext);
+  return (
+    <button style={{ background: theme.background, color: theme.foreground }}>
+      I am styled by theme context!
+    </button>
+  );
+}
+
 export default function App({ Component, pageProps }) {
   // [liffObject, profile]
   const [[liffObject, profile], setLiffState] = useState([null, null]);
   useEffect(() => {
-    if (!pageProps.liffId) return;
-    import("@line/liff").then((liff) => {
+    if(!pageProps.liffId)return;
+    import('@line/liff').then((liff) => {
       liff
         .init({ liffId: pageProps.liffId })
         .then(() => {
@@ -23,23 +81,15 @@ export default function App({ Component, pageProps }) {
                 console.warn({ err })
               })
           } else {
-            setLiffState([liff, null]);
+            setLiffState([liff, null])
           }
         })
         .catch((err) => {
-          console.warn({ err });
-        });
-    });
-  }, []);
-  return (
-    <LiffContext.Provider
-      value={{
-        liffObject: liffObject,
-        profile: profile,
-        setLiffState: setLiffState,
-      }}
-    >
-      <Component {...pageProps} />
-    </LiffContext.Provider>
-  );
+          console.warn({ err })
+        })
+    })
+  }, [profile])
+  return <LiffContext.Provider value={{liffObject: liffObject, profile: profile, setLiffState: setLiffState}}>
+    <Component {...pageProps} />
+  </LiffContext.Provider>
 }
--- pages/index.js
+++ pages/index.js
@@ -14,178 +14,21 @@ import styles from '../components/layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import { getPreviousReservations, dateToString } from "../lib/util";
 
-export default function Home({ _staffs, serviceDomain, apiKey }) {
+export default function Home({ /* ... */ }) {
   const { liffObject: liff, profile, setLiffState } = useContext(LiffContext);
 
-  const [staffs, setStaff] = useState(_staffs)
-  const [reservations, setReservation] = useState([])
-  const [snackMessage, setSnackMessage] = useState(undefined)
-
-  const microcmsClient = createMicrocmsClient({
-    serviceDomain: serviceDomain,
-    apiKey: apiKey
-  })
-  // reference: https://document.microcms.io/content-api/get-list-contents#hf768a2fd4d
-  useEffect(() => {
-    microcmsClient.get({
-      endpoint: "reservations",
-      queries: { limit: 20, filters: `lineId[equals]${profile?.userId}`, orders: '-reservationAt' },
-    }).then((data) => {
-      setReservation(data.contents)
-    })
-  }, [profile])
-
-  const previousReservations = getPreviousReservations(reservations)
-
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      {profile && (
-        <header className={styles.header}>
-          <Image
-            priority
-            src={profile.pictureUrl}
-            className={utilStyles.borderCircle}
-            height={144}
-            width={144}
-            alt={profile.displayName}
-          />
-          <h1 className={utilStyles.headingMd}>{profile.displayName}</h1>
-          <p className={utilStyles.lightText}>
-            こんにちは、{profile.displayName}さん、しげサロンへようこそ！<br/>
-
-            {
-              previousReservations.length != 0 ?
-              <>
-                来店ポイント {previousReservations.length} pt です。
-                前回の来店は {dateToString(previousReservations[0].reservationAt)} です。
-              </> :
-              `はじめまして、お客様に合った最高のヘアスタイルをご提供できるよう、スタッフ一同心よりお待ちしております。`
-            }
-            <br/>
-            <br />
-          </p>
-        </header>
-      )}
-      <Container sx={{ marginBottom: 5 }}>
-        <h2>スタッフ一覧</h2>
-        <List>
-          {staffs.map((staff) => (
-            <ListItem
-              key={staff.id}
-              secondaryAction={
-                <IconButton
-                  onClick={() => {
-                    // workshop: deleteStaff を追加しましょう
-                    if(!confirm("スタッフを削除しますか？"))return;
-                    microcmsClient
-                      .delete({
-                        endpoint: 'staffs',
-                        contentId: staff.id,
-                      })
-                      .then(() => {
-                        const newStaffs = staffs.filter((_staff) => _staff.id != staff.id)
-                        setStaff(newStaffs)
-                        setSnackMessage(`${staff.staffName}を削除しました`)
-                      })
-                      .catch((err) => console.error(err));
-                  }}
-                  aria-label=""
-                >
-                  <DeleteIcon />
-                </IconButton>
-              }
-            >
-              <Link href={`/staffs?id=${staff.id}`}>{staff.staffName}</Link>
-            </ListItem>
-          ))}
-        </List>
+  return <Layout home user={profile}>
+    <Container>
+      { profile ?
         <Button
-          variant="contained"
-          onClick={() => {
-            const staff = createRandomStaff()
-            createStaff(microcmsClient, (res) => {
-              setStaff([{ id: res.id, ...staff }, ...staffs])
-            }, staff);
-            setSnackMessage(`${staff.staffName}を追加しました`)
-          }}
-        >
-          ハンズオン用にmicroCMS上に新規スタッフ作成
-        </Button>
-      </Container>
-
-      <Container>
-        { profile ?
-          <Button
-            variant="contained"
-            color='error'
-            onClick={() => {
-              liff.logout()
-              setLiffState([liff, null])
-            }}
-          >ログアウト</Button> :
-          <Button
-            variant="contained"
-            onClick={() => {
-              liff.login({})
-            }}
-          >ログイン</Button>
-        }
-      </Container>
-
-      { /* ログイン済み */
-        profile && <Container>
-          <h2>予約一覧</h2>
-          <List>
-            {reservations.map((reservation) => (
-              <ListItem
-                key={reservation.id}
-                secondaryAction={
-                  <IconButton
-                    onClick={() => {
-                      deleteReservation(microcmsClient, reservation, () => {
-                        const date = dateToString(reservation.reservationAt)
-                        const staffName = reservation.staff.staffName;
-                        const message = `${staffName}さん：${reservation.userName}様の${date}からの予約削除がされました。`
-                        const userMessage = `${date}からの予約削除がされました。`
-                        lineNotify(message);
-                        setSnackMessage(userMessage);
-                        const _reservations = reservations.filter((e) => e !== reservation)
-                        setReservation(_reservations)
-                      });
-                    }}
-                    aria-label=""
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                }
-              >
-                <Link href={`/reservations?id=${reservation.id}`}>
-                  {reservation.staff?.staffName}: {dateToString(reservation.reservationAt)}
-                </Link>
-              </ListItem>
-            ))}
-          </List>
-        </Container>
+          onClick={() => { liff.logout(); setLiffState([liff, null]) }}
+        >ログアウト</Button> :
+        <Button
+          onClick={() => { liff.login({}) }}
+        >ログイン</Button>
       }
-
-      {
-        <Snackbar
-          open={!!snackMessage}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'right',
-          }}
-        >
-          <Alert onClose={()=>{setSnackMessage(undefined)}} severity="success">
-            {snackMessage}
-          </Alert>
-        </Snackbar>
-      }
-    </Layout>
-  )
+    </Container>
+  </Layout>
 }
 
 // データをテンプレートに受け渡す部分の処理を記述します
